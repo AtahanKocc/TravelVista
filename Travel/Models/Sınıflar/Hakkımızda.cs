2a3fc89d6a08e31a25f9a6bf259a63cd6e235d34
Atahan Koc <koccata@gmail.com>
2023-07-23 21:01:41 +0300

    added codefirst

---
+++ Travel/Models/Sınıflar/Hakkımızda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Travel.Models.Sınıflar {
+    public class Hakkımızda { // görsel,id, icerik
+        [Key]
+        public int ID { get; set; }
+        public string FotoUrl { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
